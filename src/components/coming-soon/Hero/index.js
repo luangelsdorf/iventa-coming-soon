5c54c1fa78a8d259567a501cf9207e72ddbd165c
luangelsdorf <luanferreira2136@gmail.com>
2023-04-27 08:05:04 -0300

    Initial project structure

---
+++ src/components/coming-soon/Hero/index.js
@@ -0,0 +1,7 @@
+import styles from './Hero.module.scss';
+
+export default function Hero({ content }) {
+  return (
+    <div>Hero</div>
+  )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import '@/styles/style.scss';
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import { Montserrat } from '@next/font/google';
+
+const montserrat = Montserrat({
+  weight: ['500', '600'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${montserrat.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  )
 }
