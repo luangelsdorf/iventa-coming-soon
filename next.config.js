8839cdfab4a31793ec705a1d412cd40a6120963b
luangelsdorf <luanferreira2136@gmail.com>
2023-05-23 09:08:10 -0300

    Updated next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   output: process.env.NEXT_OUTPUT,
 
+  images: {
+    unoptimized: true,
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
